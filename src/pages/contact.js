17f1a833356dd3b50806c8306951a29f2cbd5de9
Pulse6 <marco698698@hotmail.com>
2020-05-04 09:42:23 -0400

    removed favicon for now

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,9 +11,9 @@ const SecondPage = () => (
       <h1>I'm always happy to hear from you.</h1>
       <ul>
         <li><a href="mailto:marco.hl.siu@gmail.com">Email</a></li>
-        <li><a href="https://www.linkedin.com/in/marco-siu-790802162/" target="_blank">LinkedIn</a></li>
-        <li><a href="https://github.com/Pulse6" target="_blank">Github</a></li>
-        <li><a href="https://www.canva.com/design/DAD5ca8VaMA/ebM48TwW5I4CTXurvfjnSg/view?utm_content=DAD5ca8VaMA&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton" target="_blank">Resume</a></li>
+        <li><a href="https://www.linkedin.com/in/marco-siu-790802162/" rel="noopener noreferrer" target="_blank">LinkedIn</a></li>
+        <li><a href="https://github.com/Pulse6" rel="noopener noreferrer" target="_blank">Github</a></li>
+        <li><a href="https://www.canva.com/design/DAD5ca8VaMA/ebM48TwW5I4CTXurvfjnSg/view?utm_content=DAD5ca8VaMA&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton" rel="noopener noreferrer" target="_blank">Resume</a></li>
       </ul>
     </div>
   </Layout>
