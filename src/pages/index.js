cc472d1c454029a184be04eb87f8d3b48d6c237c
Pulse6 <marco698698@hotmail.com>
2020-05-05 13:26:11 -0400

    change title for index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="About" />
     <div className="about">
       <div className="img">
         <Image />
