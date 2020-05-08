d6e79f2dfa11e73833f8abcb36e837b2b645bd20
Pulse6 <marco698698@hotmail.com>
2020-05-08 12:00:45 -0400

    got useState to work in play.js

--- src/pages/play.js
+++ src/pages/play.js
@@ -3,99 +3,31 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-// const PlayPage = () => (
-//   state = {
-//     firstName: "",
-//     lastName: "",
-//   }
+const PlayPage = () => {
+  const [name, setName] = React.useState('')
 
-//   handleInputChange = event => {
-//   const target = event.target
-//   const value = target.value
-//   const name = target.name
-
-//   this.setState({
-//     [name]: value
-//   })
-// }
-
-// handleSubmit = event => {
-//   event.preventDefault()
-//   alert(`Welcome ${this.state.firstName} ${this.state.lastName}!`)
-// }
-
-// <Layout>
-//   <SEO title="404: Not found" />
-//   <form onSubmit={this.handleSubmit}>
-//     <h1>{this.state.firstName}</h1>
-//     <label>
-//       First name
-//           <input
-//         type="text"
-//         name="firstName"
-//         value={this.state.firstName}
-//         onChange={this.handleInputChange}
-//       />
-//     </label>
-//     <label>
-//       Last name
-//           <input
-//         type="text"
-//         name="lastName"
-//         value={this.state.lastName}
-//         onChange={this.handleInputChange}
-//       />
-//     </label>
-//     <button type="submit">Submit</button>
-//   </form>
-// </Layout>
-// )
-// export default PlayPage
-
-export default class PlayPage extends React.Component {
-  state = {
-    firstName: "",
-    lastName: "",
-  }
-  handleInputChange = event => {
-    const target = event.target
-    const value = target.value
-    const name = target.name
-    this.setState({
-      [name]: value,
-    })
-  }
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault()
-    alert(`Welcome ${this.state.firstName} ${this.state.lastName}!`)
+    alert(`Welcome ${name}!`)
   }
-  render() {
-    return (
-      <Layout>
-        <SEO title="Play" />
-        <h1>{this.state.firstName}</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Type Something:
+
+  return (
+    < Layout >
+      <SEO title="Play" />
+      <form onSubmit={handleSubmit}>
+        <h1>{name}</h1>
+        <label>
+          Type something:
           <input
-              type="text"
-              name="firstName"
-              value={this.state.firstName}
-              onChange={this.handleInputChange}
-            />
-          </label>
-          <label>
-            Last name
-          <input
-              type="text"
-              name="lastName"
-              value={this.state.lastName}
-              onChange={this.handleInputChange}
-            />
-          </label>  
-          <button type="submit">Submit</button>
-        </form>
-      </Layout>
-    )
-  }
+            type="text"
+            name="name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </Layout >
+  )
 }
+export default PlayPage
