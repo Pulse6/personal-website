19f366ae6dddf07cc689e6da69a65e85062671a0
Pulse6 <marco698698@hotmail.com>
2020-05-11 19:32:38 -0400

    change description in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Hok Lun Siu (Marco)`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Hok Lun Siu's personal website made with gatsby`,
     author: `Hok Lun Siu`,
   },
   plugins: [
