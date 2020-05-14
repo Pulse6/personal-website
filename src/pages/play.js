d6452345c63bd629acd7df6e15674e6fe88dcd79
Pulse6 <marco698698@hotmail.com>
2020-05-14 12:26:09 -0400

    will load starwar's name

--- src/pages/play.js
+++ src/pages/play.js
@@ -6,10 +6,12 @@ import SEO from "../components/seo"
 
 const PlayPage = () => {
   const [name, setName] = useState('')
+  const [starwar, setStarwar] = useState(null)
 
   React.useEffect(() => {
-    axios.get('https://pokeapi.co/api/v2/pokemon/ditto/')
-    .then((res) => console.log("THISSS",res))
+    // axios.get('https://pokeapi.co/api/v2/pokemon/ditto/')
+    axios.get('https://swapi.dev/api/people/')
+    .then((res) => setStarwar(res.data.results))
   }, [])
 
   const handleSubmit = event => {
@@ -34,6 +36,9 @@ const PlayPage = () => {
           {/* <button type="submit">Submit</button> */}
         </form>
       </div>
+      {!starwar ? <div>Loading...</div> : starwar.map((person) => {
+        return <div key={person.name}>{person.name}</div>
+      })}
     </Layout >
   )
 }
