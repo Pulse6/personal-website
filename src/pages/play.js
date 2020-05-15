e74a0709b98bb92424b001bb7e91be7ce87ab0d0
Pulse6 <marco698698@hotmail.com>
2020-05-14 21:46:41 -0400

    got pokimon to show with their skills

--- src/pages/play.js
+++ src/pages/play.js
@@ -6,17 +6,28 @@ import SEO from "../components/seo"
 
 const PlayPage = () => {
   const [name, setName] = useState('')
-  const [starwar, setStarwar] = useState(null)
+  const [show, setShow] = useState(false)
+  const [poki, setPoki] = useState(null)
+  // const [starwar, setStarwar] = useState(null)
 
-  React.useEffect(() => {
-    // axios.get('https://pokeapi.co/api/v2/pokemon/ditto/')
-    axios.get('https://swapi.dev/api/people/')
-    .then((res) => setStarwar(res.data.results))
-  }, [])
+  // React.useEffect(() => {
+  //   // axios.get(`https://pokeapi.co/api/v2/pokemon?limit=964`)
+  //   axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+  //   // axios.get('https://swapi.dev/api/people/')
+  //   .then((res) => console.log(res.data))
+  // }, [])
 
   const handleSubmit = event => {
     event.preventDefault()
-    alert(`Welcome ${name}!`)
+    // alert(`Welcome ${name}!`)
+    // axios.get(`https://pokeapi.co/api/v2/pokemon?limit=964`)
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      // axios.get('https://swapi.dev/api/people/')
+      .then((res) => {
+        setPoki(res.data)
+        console.log(res.data)
+      })
+    setShow(true)
   }
 
   return (
@@ -24,7 +35,6 @@ const PlayPage = () => {
       <SEO title="Play" />
       <div className="playground">
         <form onSubmit={handleSubmit}>
-          <h1>{name}</h1>
           <label>
             <input
               type="text"
@@ -33,12 +43,25 @@ const PlayPage = () => {
               onChange={(event) => setName(event.target.value)}
             />
           </label>
-          {/* <button type="submit">Submit</button> */}
+          <button type="submit">Submit</button>
         </form>
       </div>
-      {!starwar ? <div>Loading...</div> : starwar.map((person) => {
+      {/* {!starwar ? <div>Loading...</div> : starwar.map((person) => {
         return <div key={person.name}>{person.name}</div>
-      })}
+      })} */}
+      {/* {!poki ? <p>Loading...</p> : poki.map(eachpoki =>{
+        return <p key={eachpoki.name}>{eachpoki.name}</p>
+      })} */}
+      {show && poki &&
+        <>
+          <img src={poki.sprites.front_default} alt={poki.name}/>
+          <h3>{poki.name}</h3>
+          <h4>Abilities</h4>
+          {poki.abilities.map(skills => {
+            return <p>{skills.ability.name}</p>
+          })}
+        </>
+      }
     </Layout >
   )
 }
