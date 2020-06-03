c8857a1f7bf894b55c6c3bd708cda788afe4f10c
Pulse6 <marco698698@hotmail.com>
2020-06-03 11:47:20 -0400

    ingredients are showing

--- src/pages/play.js
+++ src/pages/play.js
@@ -6,11 +6,14 @@ import SEO from "../components/seo"
 
 const PlayPage = () => {
   const [input, setInput] = useState('')
-  // const [show, setShow] = useState(false)
+  const [loading, setLoading] = useState(false)
   const [data, setData] = useState(null)
   const [err, setErr] = useState(false)
+  // const [show, setShow] = useState(false)
 
   const handleSubmit = event => {
+    setData(null)
+    setLoading(true)
     event.preventDefault()
     try {
       axios.get(`https://api.edamam.com/search?q=${input}&app_id=${process.env.GATSBY_APP_RECIPE_SEARCH_ID}&app_key=${process.env.GATSBY_APP_RECIPE_SEARCH_KEY}`)
@@ -18,6 +21,7 @@ const PlayPage = () => {
           console.log(res.data)
           setData(res.data)
           setErr(false)
+          setLoading(false)
         })
     }
     catch (error) {
@@ -42,9 +46,17 @@ const PlayPage = () => {
           <button type="submit">Submit</button>
         </form>
       </div>
+      {loading && <div>Loading...</div>}
       {err && <div>Something Went Wrong :o</div>}
       {!err && data && data.hits.map(item => {
-        return <div key={item.recipe.label}>{item.recipe.label}</div>
+        return (
+          <div key={item.recipe.label}>
+            <div>{item.recipe.label}</div>
+            {item.recipe.ingredientLines.map(item => {
+              return <div key={item}>- {item}</div>
+            })}
+          </div>
+        )
       })}
     </Layout >
   )
